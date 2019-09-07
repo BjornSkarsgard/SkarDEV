2b7ad4e984a46ad369ac26569813adcceaba9a92
Bjorn <bjornskarsgarddev@gmail.com>
2019-09-08 03:05:09 +0500

    Сделал крутую анимацию на странице портфолио

--- source/js/dist/grade.js
+++ source/js/dist/grade.js
@@ -1,15 +1,45 @@
-(function (f) { if (typeof exports === "object" && typeof module !== "undefined") { module.exports = f() } else if (typeof define === "function" && define.amd) { define([], f) } else { var g; if (typeof window !== "undefined") { g = window } else if (typeof global !== "undefined") { g = global } else if (typeof self !== "undefined") { g = self } else { g = this } g.Grade = f() } })(function () {
-    var define, module, exports; return (function e(t, n, r) { function s(o, u) { if (!n[o]) { if (!t[o]) { var a = typeof require == "function" && require; if (!u && a) return a(o, !0); if (i) return i(o, !0); var f = new Error("Cannot find module '" + o + "'"); throw f.code = "MODULE_NOT_FOUND", f } var l = n[o] = { exports: {} }; t[o][0].call(l.exports, function (e) { var n = t[o][1][e]; return s(n ? n : e) }, l, l.exports, e, t, n, r) } return n[o].exports } var i = typeof require == "function" && require; for (var o = 0; o < r.length; o++)s(r[o]); return s })({
-        1: [function (require, module, exports) {
+(function(f) {
+    if (typeof exports === "object" && typeof module !== "undefined") { module.exports = f() } else if (typeof define === "function" && define.amd) { define([], f) } else {
+        var g;
+        if (typeof window !== "undefined") { g = window } else if (typeof global !== "undefined") { g = global } else if (typeof self !== "undefined") { g = self } else { g = this }
+        g.Grade = f()
+    }
+})(function() {
+    var define, module, exports;
+    return (function e(t, n, r) {
+        function s(o, u) {
+            if (!n[o]) {
+                if (!t[o]) { var a = typeof require == "function" && require; if (!u && a) return a(o, !0); if (i) return i(o, !0); var f = new Error("Cannot find module '" + o + "'"); throw f.code = "MODULE_NOT_FOUND", f }
+                var l = n[o] = { exports: {} };
+                t[o][0].call(l.exports, function(e) { var n = t[o][1][e]; return s(n ? n : e) }, l, l.exports, e, t, n, r)
+            }
+            return n[o].exports
+        }
+        var i = typeof require == "function" && require;
+        for (var o = 0; o < r.length; o++) s(r[o]);
+        return s
+    })({
+        1: [function(require, module, exports) {
             'use strict';
 
-            var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+            var _createClass = function() {
+                function defineProperties(target, props) {
+                    for (var i = 0; i < props.length; i++) {
+                        var descriptor = props[i];
+                        descriptor.enumerable = descriptor.enumerable || false;
+                        descriptor.configurable = true;
+                        if ("value" in descriptor) descriptor.writable = true;
+                        Object.defineProperty(target, descriptor.key, descriptor);
+                    }
+                }
+                return function(Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; };
+            }();
 
             function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
             var prefixes = ['webkit'];
 
-            var Grade = function () {
+            var Grade = function() {
                 function Grade(container, img_selector, callback) {
                     _classCallCheck(this, Grade);
 
@@ -48,7 +78,7 @@
                     value: function getChunkedImageData() {
                         var perChunk = 4;
 
-                        var chunked = this.imageData.reduce(function (ar, it, i) {
+                        var chunked = this.imageData.reduce(function(ar, it, i) {
                             var ix = Math.floor(i / perChunk);
                             if (!ar[ix]) {
                                 ar[ix] = [];
@@ -57,10 +87,10 @@
                             return ar;
                         }, []);
 
-                        var filtered = chunked.filter(function (rgba) {
-                            return rgba.slice(0, 2).every(function (val) {
+                        var filtered = chunked.filter(function(rgba) {
+                            return rgba.slice(0, 2).every(function(val) {
                                 return val < 250;
-                            }) && rgba.slice(0, 2).every(function (val) {
+                            }) && rgba.slice(0, 2).every(function(val) {
                                 return val > 0;
                             });
                         });
@@ -70,7 +100,7 @@
                 }, {
                     key: 'getRGBAGradientValues',
                     value: function getRGBAGradientValues(top) {
-                        return top.map(function (color, index) {
+                        return top.map(function(color, index) {
                             return 'rgb(' + color.rgba.slice(0, 3).join(',') + ') ' + (index == 0 ? '0%' : '75%');
                         }).join(',');
                     }
@@ -78,7 +108,7 @@
                     key: 'getCSSGradientProperty',
                     value: function getCSSGradientProperty(top) {
                         var val = this.getRGBAGradientValues(top);
-                        return prefixes.map(function (prefix) {
+                        return prefixes.map(function(prefix) {
                             return 'background-image: -' + prefix + '-linear-gradient(\n                        135deg,\n                        ' + val + '\n                    )';
                         }).concat(['background-image: linear-gradient(\n                    135deg,\n                    ' + val + '\n                )']).join(';');
                     }
@@ -94,7 +124,7 @@
                 }, {
                     key: 'getSortedValues',
                     value: function getSortedValues(uniq) {
-                        var occurs = Object.keys(uniq).map(function (key) {
+                        var occurs = Object.keys(uniq).map(function(key) {
                             var rgbaKey = key;
                             var components = key.split('|'),
                                 brightness = (components[0] * 299 + components[1] * 587 + components[2] * 114) / 1000;
@@ -103,10 +133,10 @@
                                 occurs: uniq[key],
                                 brightness: brightness
                             };
-                        }).sort(function (a, b) {
+                        }).sort(function(a, b) {
                             return a.occurs - b.occurs;
                         }).reverse().slice(0, 10);
-                        return occurs.sort(function (a, b) {
+                        return occurs.sort(function(a, b) {
                             return a.brightness - b.brightness;
                         }).reverse();
                     }
@@ -130,7 +160,7 @@
                 }, {
                     key: 'getUniqValues',
                     value: function getUniqValues(chunked) {
-                        return chunked.reduce(function (accum, current) {
+                        return chunked.reduce(function(accum, current) {
                             var key = current.join('|');
                             if (!accum[key]) {
                                 accum[key] = 1;
@@ -184,7 +214,7 @@
                 return Grade;
             }();
 
-            module.exports = function (containers, img_selector, callback) {
+            module.exports = function(containers, img_selector, callback) {
                 var init = function init(container, img_selector, callback) {
                     var grade = new Grade(container, img_selector, callback),
                         gradientData = grade.gradientData;
@@ -196,7 +226,7 @@
                         gradientData: gradientData
                     };
                 };
-                var results = (NodeList.prototype.isPrototypeOf(containers) ? Array.from(containers).map(function (container) {
+                var results = (NodeList.prototype.isPrototypeOf(containers) ? Array.from(containers).map(function(container) {
                     return init(container, img_selector, callback);
                 }) : [init(containers, img_selector, callback)]).filter(Boolean);
 
